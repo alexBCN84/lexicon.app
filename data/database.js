93a5a68afdbce2fe72e36711f95adcc5bb0e4e15
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2017-11-02 09:43:24 +0100

    solving syntax error issues

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const stringify = require('json-stringify-safe');
 
-module.exports.saveFile = async(filename, component) => {
+module.exports.saveFile = (filename, component) => {
     return new Promise((resolve, reject) => {
         fs.writeFile(filename, stringify(component, null, 1), (err, contents) => {
             if (err) return reject(err);
@@ -12,7 +12,7 @@ module.exports.saveFile = async(filename, component) => {
 };
 
 
-module.exports.readFile = async(filename) => {
+module.exports.readFile = (filename) => {
     return new Promise((resolve, reject) => {
         fs.readFile(filename, 'utf8', (err, contents) => {
             if (err) return reject(err);
