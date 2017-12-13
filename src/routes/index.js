b1bc18fe00425afe11c012cb107bfe96db64a757
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2017-12-13 10:17:16 +0100

    page title is no longer undefined when page loads

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Welcome'
+    });
+});
+
 router.get('/index', (req, res) => {
     res.render('index', {
         title: 'Home'
