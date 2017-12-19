d5d8cabfe9c3a2fc4af84cbe89ec4aa871cf83df
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2017-12-19 13:04:53 +0100

    quote styled up

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 // Some details about the site
-exports.siteName = `LEXICON`;
+const siteName = `LEXICON`;
 
 const menu = [
     { slug: '/index', title: 'Home' },
@@ -10,4 +10,43 @@ const menu = [
 
 const models = menu.filter(model => model.title !== 'Home')
 
-module.exports = { menu, models }
+const motto = 'Enlarge your world by empowering your knowledge'
+const userFeatures = [
+    { feature: 'Create and share your online profile with your skills and interests.' },
+    { feature: 'Look for other users, like them, follow them.' },
+    { feature: 'Share your glossaries and entries with other users.' },
+    { feature: 'Create and manage your own work group.' },
+    { feature: 'Contribute to group projects.' },
+    { feature: 'Review and rate other members\' glossaries and entries.' },
+    { feature: 'Create your own vocabulary lists and set study sessions.' }
+]
+
+const glossaryFeatures = [
+    { feature: 'Create and share your online profile with your skills and interests.' },
+    { feature: 'Look for other users, like them, follow them.' },
+    { feature: 'Share your glossaries and entries with other users.' },
+    { feature: 'Create and manage your own work group.' },
+    { feature: 'Contribute to group projects.' },
+    { feature: 'Review and rate other members\' glossaries and entries.' },
+    { feature: 'Create your own vocabulary lists and set study sessions.' }
+]
+
+const entryFeatures = [
+    { feature: 'Create and share your online profile with your skills and interests.' },
+    { feature: 'Look for other users, like them, follow them.' },
+    { feature: 'Share your glossaries and entries with other users.' },
+    { feature: 'Create and manage your own work group.' },
+    { feature: 'Contribute to group projects.' },
+    { feature: 'Review and rate other members\' glossaries and entries.' },
+    { feature: 'Create your own vocabulary lists and set study sessions.' }
+]
+
+module.exports = {
+    siteName,
+    menu,
+    motto,
+    models,
+    userFeatures,
+    glossaryFeatures,
+    entryFeatures
+}
